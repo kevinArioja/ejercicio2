6fc1803fed4bbf1429d2505a2ccadce5af80852f
kevinArioja <145269693+kevinArioja@users.noreply.github.com>
2023-09-28 22:56:51 -0400

    Corrección de errores del merge

--- aritmetica.js
+++ aritmetica.js
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
+
 function suma(a, b){
 	return a + b;
 }
 console.log(suma(10, 7));
-=======
+
 function multiplicacion(a, b) {
     return a * b;
 }
@@ -12,4 +12,4 @@ const numero1 = parseFloat(prompt('Ingresa el primer número:'));
 const numero2 = parseFloat(prompt('Ingresa el segundo número:'));
 
 console.log(`Resultado de la multiplicación: ${multiplicacion(numero1, numero2)}`);
->>>>>>> 10be2f5c51a4386b71e6210bca66c72ff7895a0f
+
